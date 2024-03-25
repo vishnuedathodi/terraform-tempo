cfaa06dba1a42eca62a3d328ff35d3207bb9384f
vishnuedathodi <154342576+vishnuedathodi@users.noreply.github.com>
2024-03-25 17:27:12 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,5 +1,8 @@
 variable "cluster_name" {
   description = "cluster name"
   type        = string
-  default     = "tg-tekton-eks-cluster"
+}
+variable "release_name" {
+  description = "Helm release name"
+  type        = string
 }
